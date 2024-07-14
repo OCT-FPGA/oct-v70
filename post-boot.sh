317b8f501a13e5e343d128d62cd86cf10d33607d
Suranga Mahesh <72818884+surangamh@users.noreply.github.com>
2024-07-13 20:36:21 -0400

    Update post-boot.sh

--- post-boot.sh
+++ post-boot.sh
@@ -24,7 +24,11 @@ echo "Home directory:"
 HOMEDIR="/users/$USER"
 echo "$HOMEDIR"
 REPO_URL="https://github.com/OCT-FPGA/Vitis-AI"
-bash -c "cd '$HOMEDIR' && git clone '$REPO_URL' && cd Vitis-AI/board_setup/v70; source install.sh"
+
+docker_dir = "/docker"
+sudo chmod 755 $docker_dir
+sudo chown $USER:octfpga-PG0 docker_dir
+bash -c "cd '$docker_dir' && git clone '$REPO_URL' && cd Vitis-AI/board_setup/v70; source install.sh"
 
 sudo usermod -aG docker $USER
 newgrp docker
